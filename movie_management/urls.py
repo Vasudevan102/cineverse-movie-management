ab547ba4cbe7476a774f817b415c79fcb6749b40
Vasudevan <rvasudevan102@gmail.com>
2026-08-15 21:24:43 +0530

    fix(production): fix production static asset delivery and media file routing for Vercel deployment

--- movie_management/urls.py
+++ movie_management/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from movies.views import home_view
 
 urlpatterns = [
@@ -11,8 +12,9 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('booking/', include('booking.urls')),
     path('reviews/', include('reviews.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
